e9b20d6f594c12fabe2440fc5648b21a013d50ad
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-04-29 16:55:38 -0300

    Novos testes de métodos foram adicionados

--- bytebank/bytebank/Program.cs
+++ bytebank/bytebank/Program.cs
@@ -1,14 +1,39 @@
 ﻿using bytebank;
 
+//Teste criação da conta 1
 ContaCorrente contaAndre = new ContaCorrente();
 contaAndre.titular = "André Silva";
 contaAndre.numeroAgencia = 15;
 contaAndre.conta = "1010-X";
 contaAndre.saldo = 100.0;
 
-Console.WriteLine($"Saldo na conta do André = {contaAndre.saldo}");
+Console.WriteLine($"Saldo na conta do André = R$ {String.Format("{0:0.00}", contaAndre.saldo)}");
 
-contaAndre.Depositar(100);
-Console.WriteLine($"Saldo da conta do André após o depósito é de R$ {contaAndre.saldo},00");
+//Teste método depositar
+/*contaAndre.Depositar(100);
+Console.WriteLine($"Saldo da conta do André após o depósito é de R$ {String.Format("{0:0.00}", contaAndre.saldo)}");
+
+//Teste método sacar (com validação)
+if(contaAndre.Sacar(300) == true)
+{ Console.WriteLine($"Saldo atual {String.Format("{0:0.00}", contaAndre.saldo)}");
+}
+else
+{
+    Console.WriteLine($"Não foi possível realizar o saque pois saldo em conta é de R$ {String.Format("{0:0.00}", contaAndre.saldo)}");
+}*/
+
+//Teste criação conta 2
+ContaCorrente contaMaria = new ContaCorrente();
+contaMaria.titular = "Maria Souza";
+contaMaria.numeroAgencia = 17;
+contaMaria.conta = "1010-5";
+contaMaria.saldo = 350.0;
+
+Console.WriteLine($"Saldo da conta da Maria = R$ {String.Format("{0:0.00}", contaMaria.saldo)}");
+
+//Teste transferir
+contaAndre.Transferir(50, contaMaria);
+Console.WriteLine($"Novo saldo da conta da Maria = R$ {String.Format("{0:0.00}", contaMaria.saldo)}");
+Console.WriteLine($"Novo saldo da conta do André = R$ {String.Format("{0:0.00}", contaAndre.saldo)}");
 
 Console.ReadKey();
