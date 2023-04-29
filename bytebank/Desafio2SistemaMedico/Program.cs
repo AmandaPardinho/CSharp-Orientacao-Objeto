37017c03e4faf45035ec86efe49d37951cf3a198
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-04-29 17:14:27 -0300

    Desafio 2 - criação do objeto pessoa e atribuição de valores a variaveis da classe Pessoa

---
+++ bytebank/Desafio2SistemaMedico/Program.cs
@@ -0,0 +1,12 @@
+﻿using Desafio2SistemaMedico;
+
+Pessoa pessoa1 = new Pessoa();
+
+pessoa1.nome = "Beatriz Oliveira";
+pessoa1.idade = 23;
+pessoa1.altura = 1.61;
+pessoa1.peso = 73;
+
+Console.WriteLine($"Nome: {pessoa1.nome}\nIdade: {pessoa1.idade} anos\nAltura: {pessoa1.altura}m\nPeso: {pessoa1.peso} kg");
+
+Console.ReadKey();
