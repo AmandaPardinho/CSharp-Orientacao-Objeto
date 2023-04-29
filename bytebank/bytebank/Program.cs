30ea5287328c112b79b142349654dee38cc402c4
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-04-29 18:28:37 -0300

    Inclusão de código exemplificando e mostrando a diferença entretipagem por valor e tipagem por referência

--- bytebank/bytebank/Program.cs
+++ bytebank/bytebank/Program.cs
@@ -6,41 +6,67 @@ contaAndre.titular = "André Silva";
 contaAndre.numeroAgencia = 15;
 contaAndre.conta = "1010-X";
 contaAndre.saldo = 100.0;
-
 Console.WriteLine($"Saldo na conta do André = R$ {String.Format("{0:0.00}", contaAndre.saldo)}");
 
-//Teste método depositar
-/*contaAndre.Depositar(100);
-Console.WriteLine($"Saldo da conta do André após o depósito é de R$ {String.Format("{0:0.00}", contaAndre.saldo)}");
+ContaCorrente contaAndre2 = new ContaCorrente();
+contaAndre2.titular = "André Silva";
+contaAndre2.numeroAgencia = 15;
+contaAndre2.conta = "1010-X";
+contaAndre2.saldo = 100.0;
+Console.WriteLine($"Saldo na conta do André 2 = R$ {String.Format("{0:0.00}", contaAndre2.saldo)}");
 
-//Teste método sacar (com validação)
-if(contaAndre.Sacar(300) == true)
-{ Console.WriteLine($"Saldo atual {String.Format("{0:0.00}", contaAndre.saldo)}");
-}
-else
-{
-    Console.WriteLine($"Não foi possível realizar o saque pois saldo em conta é de R$ {String.Format("{0:0.00}", contaAndre.saldo)}");
-}*/
+Console.WriteLine(contaAndre == contaAndre2);
 
-//Teste criação conta 2
-ContaCorrente contaMaria = new ContaCorrente();
-contaMaria.titular = "Maria Souza";
-contaMaria.numeroAgencia = 17;
-contaMaria.conta = "1010-5";
-contaMaria.saldo = 350.0;
+////Teste método depositar
+///*contaAndre.Depositar(100);
+//Console.WriteLine($"Saldo da conta do André após o depósito é de R$ {String.Format("{0:0.00}", contaAndre.saldo)}");
 
-Console.WriteLine($"Saldo da conta da Maria = R$ {String.Format("{0:0.00}", contaMaria.saldo)}");
+////Teste método sacar (com validação)
+//if(contaAndre.Sacar(300) == true)
+//{ Console.WriteLine($"Saldo atual {String.Format("{0:0.00}", contaAndre.saldo)}");
+//}
+//else
+//{
+//    Console.WriteLine($"Não foi possível realizar o saque pois saldo em conta é de R$ {String.Format("{0:0.00}", contaAndre.saldo)}");
+//}*/
 
-//Teste transferir
-contaAndre.Transferir(50, contaMaria);
-Console.WriteLine($"Novo saldo da conta da Maria = R$ {String.Format("{0:0.00}", contaMaria.saldo)}");
-Console.WriteLine($"Novo saldo da conta do André = R$ {String.Format("{0:0.00}", contaAndre.saldo)}");
+////Teste criação conta 2
+//ContaCorrente contaMaria = new ContaCorrente();
+//contaMaria.titular = "Maria Souza";
+//contaMaria.numeroAgencia = 17;
+//contaMaria.conta = "1010-5";
+//contaMaria.saldo = 350.0;
 
-//Teste criação conta 3
-ContaCorrente contaPedro = new ContaCorrente();
-contaPedro.titular = "Pedro Soares";
+//Console.WriteLine($"Saldo da conta da Maria = R$ {String.Format("{0:0.00}", contaMaria.saldo)}");
 
-//Teste valor padrão dos campos (variáveis) criadas na classe ContaCorrente
-Console.WriteLine($"Nome do titular: {contaPedro.titular}\nNúmero da agência: {contaPedro.numeroAgencia}\nNúmero da conta: {contaPedro.conta}\nSaldo: {contaPedro.saldo}");
+////Teste transferir
+//contaAndre.Transferir(50, contaMaria);
+//Console.WriteLine($"Novo saldo da conta da Maria = R$ {String.Format("{0:0.00}", contaMaria.saldo)}");
+//Console.WriteLine($"Novo saldo da conta do André = R$ {String.Format("{0:0.00}", contaAndre.saldo)}");
+
+////Teste criação conta 3
+//ContaCorrente contaPedro = new ContaCorrente();
+//contaPedro.titular = "Pedro Soares";
+
+////Teste valor padrão dos campos (variáveis) criadas na classe ContaCorrente
+//Console.WriteLine($"Nome do titular: {contaPedro.titular}\nNúmero da agência: {contaPedro.numeroAgencia}\nNúmero da conta: {contaPedro.conta}\nSaldo: {contaPedro.saldo}");
+
+/*Tipagem por valor => compara valores
+ * envolve tipos primitivos;
+ * valor e valor2 não apontam para o mesmo endereço na memória => é criada uma cópia do conteúdo da variável valor para a variável valor2; porém, caso haja uma comparação entre eles para verificar se são iguais, o resultado será TRUE ;
+ */
+double valor = 300;
+double valor2 = valor;
+Console.WriteLine(valor == valor2);
+
+/*Tipagem por referência => compara endereços na memória
+ * criação de um atalho como referência
+ * necessário o uso da palavra reservada "new" e o nome do objeto que se quer criar;
+ * necessário que haja uma referência para conseguir manipular o objeto em memória, acessar seus campos e métodos => criação, no computador, de um novo objeto; 
+ *cada objeto tem um endereçamento único na memória e, por isso, ao compará-los para ver se são iguais, o resultado será FALSE;
+ */
+
+
+
 
 Console.ReadKey();
