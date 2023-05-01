5bb04997f23f3f542e6901edcddb05fd266745ec
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-04-30 22:43:59 -0300

    Criação do código do desafio aluno

---
+++ bytebank/DesafioAluno/Aluno.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioAluno
+{
+    public class Aluno
+    {
+        private int _matricula;
+        public int Matricula
+        {
+            get { return _matricula; }
+            set
+            {
+                if(value > 0)
+                {
+                    _matricula = value;
+                }
+            }
+        }
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public bool Status { get; set; }
+
+        public int TipoCurso { get; set; }
+    }
+}
