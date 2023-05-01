ca2cc51fcb4ec554777eb145f8ac7b9521666839
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-04-30 22:33:05 -0300

    Implementação de propriedades autoimplementadas e de uma propriedade com validação

--- bytebank/bytebank/Contas/ContaCorrente.cs
+++ bytebank/bytebank/Contas/ContaCorrente.cs
@@ -12,10 +12,33 @@ namespace bytebank.Contas
         //Cada um dos campos recebe um valor inicial padrão
         //É possível alterar esse valor padrão do campo aqui mesmo na criação da classe => basta atribuir (=) um valor de acordo com o seu tipo
 
-        public int numeroAgencia;
-        public string conta;
-        public Cliente titular;
-        public double saldo;
+        private int _numeroAgencia;
+
+        //Definindo uma propriedade 
+        public int NumeroAgência
+        {
+            get { return _numeroAgencia; }
+            set
+            {
+                if(value > 0)
+                {
+                    _numeroAgencia = value;
+                }
+            }
+        }
+        
+        /* Criação de uma propriedade autoimplementada
+         * - Útil quando não há necessidade de realizar validação
+         * - Deixa o código mais enxuto
+         * - Cria um campo privado interno e atribui um valor na execução
+         */
+        //private string conta;
+        public string Conta { get; set; }
+
+        //private Cliente titular;
+        public Cliente Titular { get; set; }
+
+        private double saldo;
         //public double saldo = 100;
 
         //Método depositar
@@ -54,9 +77,28 @@ namespace bytebank.Contas
         }
 
         //Exibir informações da conta
-        public string ExibirInfos()
+        /*public string ExibirInfos()
         {
-            return $"Titular: {titular.nome}\nCPF: {titular.cpf}\nProfissão: {titular.profissao}\nNúmero da agência: {numeroAgencia}\nNúmero da conta: {conta}\nSaldo: R$ {string.Format("{0:0.00}", saldo)}";
+            return $"Titular: {titular.nome}\nCPF: {titular.cpf}\nProfissão: {titular.profissao}\nNúmero da agência: {_numeroAgencia}\nNúmero da conta: {conta}\nSaldo: R$ {string.Format("{0:0.00}", saldo)}";
+        }*/
+
+        public void SetSaldo(double valor)
+        {
+            if(valor < 0)
+            {
+                return;
+            }
+            else
+            {
+                this.saldo = valor;
+            }
+        }
+
+        public double GetSaldo()
+        {
+            return this.saldo;
         }
     }
+
+
 }
