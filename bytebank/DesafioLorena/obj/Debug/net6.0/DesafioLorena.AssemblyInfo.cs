fb0b216152e573cc4d36245b0a5ea2aa34418f73
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-04-30 22:44:30 -0300

    Modificação de arquivos criados pela IDE após compilação

--- bytebank/DesafioLorena/obj/Debug/net6.0/DesafioLorena.AssemblyInfo.cs
+++ bytebank/DesafioLorena/obj/Debug/net6.0/DesafioLorena.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("desafiolorena")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("DesafioLorena")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("desafiolorena")]
-[assembly: System.Reflection.AssemblyTitleAttribute("desafiolorena")]
+[assembly: System.Reflection.AssemblyProductAttribute("DesafioLorena")]
+[assembly: System.Reflection.AssemblyTitleAttribute("DesafioLorena")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
