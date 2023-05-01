bce60a2e44a2081138fa3f660e51c04f56306f3c
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-05-01 12:23:52 -0300

    Alterações na classe Cliente

--- bytebank/bytebank/Titular/Cliente.cs
+++ bytebank/bytebank/Titular/Cliente.cs
@@ -11,5 +11,11 @@ namespace bytebank.Titular
         public string Nome { get; set; }
         public string Cpf { get; set; }
         public string Profissao { get; set; }
+
+        //Método construtor => é invocado quando se cria um novo objeto (assim q se coloca a palavra "new")
+        public Cliente() 
+        {
+            
+        }
     }
 }
